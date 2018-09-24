1e5d86a54841f6ee5f8870ecb62e58732d52abc5
Arioch <arioch@futurolan.net>
2018-09-24 12:37:42 +0200

    Modif icons et slide

--- config/config.js
+++ config/config.js
@@ -4,9 +4,9 @@ let config = {
   image: `${process.env.BASE_URL}/static/img/logo-orange-200x200.png`,
   home: {
     hero: {
-      title: 'Gamers Assembly',
-      subtitle: 'Halloween Edition 2018',
-      background: '/static/img/bg-orange-area.jpg'
+      title: '',
+      subtitle: '',
+      background: '/static/img/bg-slide-v2-he2018.jpg'
     }
   },
   news: {
@@ -36,9 +36,9 @@ let config = {
   social: {
     twitter: 'https://twitter.com/GamersAssembly',
     facebook: 'https://www.facebook.com/GamersAssembly',
-    twitch: 'https://www.flickr.com/photos/futurolan',
-    youtube: 'https://twitch.tv/gamers_assembly',
-    flickr: 'https://www.youtube.com/channel/UCbfhRIAsc4xdRACnDUwRfRw'
+    twitch: 'https://twitch.tv/gamers_assembly',
+    youtube: 'https://www.youtube.com/channel/UCbfhRIAsc4xdRACnDUwRfRw',
+    flickr: 'https://www.flickr.com/photos/futurolan'
   }
 }
 
